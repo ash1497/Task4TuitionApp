b32df17447286ddf2c091f9aa56b8cc9c174aebc
Aashir Hussain <husaashi@sheridancollege.ca>
2022-04-13 13:06:58 -0400

    Testing.

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            REACT NATIVE IS ANNOYING
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
