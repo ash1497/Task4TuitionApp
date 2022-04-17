e7d4d4bd3af142a858ea01406971ca50cfc339ad
Zahaak Khan <khazahaa@sheridancollege.ca>
2022-04-17 17:30:59 -0400

    added a studentdata object to home js

--- displays/Home.js
+++ displays/Home.js
@@ -13,43 +13,82 @@ import {
 import {icons, images, SIZES, COLORS, FONTS} from '../constants';
 
 const Home = () => {
+  const CurrentLocation = {
+    streetName: '7042 Guildhall Court',
+    gps: {
+      latitude: 43.576879694930376,
+      longitude: -79.78592687986493,
+    },
+  };
 
-    const CurrentLocation = {
-        streetName: "7042 Guildhall Court",
-        gps: {
-            latitude: 43.576879694930376,
-            longitude: -79.78592687986493
-        }
-    }
+  const taskData = [
+    {
+      id: 1,
+      name: 'Lawn Mowing',
+      icon: icons.lawnmowing,
+    },
+    {
+      id: 2,
+      name: 'Gardening',
+      icon: icons.gardening,
+    },
+    {
+      id: 3,
+      name: 'Snow Shovelling',
+      icon: icons.snowshovel,
+    },
+    {
+      id: 4,
+      name: 'Car Washing',
+      icon: icons.carwash,
+    },
+    {
+      id: 5,
+      name: 'Miscellanous',
+      icon: icons.misc,
+    },
+  ];
 
-    const taskData = [
+  const studentData = [
+    {
+      id: 1,
+      name: 'Zahaak Khan',
+      rating: 4.8,
+      categories: [1, 4],
+      //priceRating: affordable,
+      price: 25,
+      photo: images.timmy,
+      //duration: '30 - 45 min',
+      availability: '2:00 PM - 8:00 PM',
+      location: {
+        // latitude: 1.5347282806345879,
+        //longitude: 110.35632207358996,
+        latitude: 43.394876,
+        longitude: -79.894195,
+      },
+      courier: {
+        avatar: images.avatar_1,
+        name: 'Zahaak Khan',
+      },
+      menu: [
         {
-            id: 1,
-            name: "Lawn Mowing",
-            icon: icons.lawnmowing,
+          taskId: 1,
+          name: 'Lawn Mowing',
+          photo: images.lawnmow,
+          description:
+            'Excellent technique and care. Evenly cur lawn, the best in the business.',
+          price: 25,
         },
         {
-            id: 2,
-            name: "Gardening",
-            icon: icons.gardening,
+          taskId: 2,
+          name: 'Car Wash',
+          photo: images.carwash,
+          description: 'Amazing at washing cars, tons of experience.',
+          price: 100,
         },
-        {
-            id: 3,
-            name: "Snow Shovelling",
-            icon: icons.snowshovel,
-        },
-        {
-            id: 4,
-            name: "Car Washing",
-            icon: icons.carwash,
-        },
-        {
-            id: 5,
-            name: "Miscellanous",
-            icon: icons.misc,
-        },
-    ]
-
+      ],
+    },
+  ];
 
   function renderHeader() {
     return (
@@ -107,15 +146,7 @@ const Home = () => {
     );
   }
 
-  return (
-
-    <SafeAreaView style={styles.container}>
-
-        {renderHeader()}
-
-
-    </SafeAreaView>
-    )
+  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
