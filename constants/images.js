8392626cf77badd242e0b5527217f865d0a65170
Aashir Hussain <husaashi@sheridancollege.ca>
2022-04-17 12:34:44 -0400

    Coded the index, icon, and images file.

---
+++ constants/images.js
@@ -0,0 +1,10 @@
+
+export const avatar1 = require ('../assets/images/avatar1.jpg');
+export const avatar2 = require ('../assets/images/avatar-2.jpg');
+
+
+
+export default {
+    avatar1,
+    avatar2,
+}
