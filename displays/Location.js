02e23a88fc05f41260f889d7976c40226a8fa1de
Aashir Hussain <husaashi@sheridancollege.ca>
2022-04-17 13:21:59 -0400

    Started Coding the Displays.

---
+++ displays/Location.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {
+    View,
+    Text
+} from "react-native";
+
+const Location = () => {
+    return (
+        <view>
+            <text>Location</text>
+        </view>
+    )
+}
+
+export default Location;
---
+++ displays/index.js
@@ -0,0 +1,9 @@
+import Home from './Home';
+import Location from './Location';
+import StudentInfo from './StudentInfo';
+
+export {
+    Home,
+    Location,
+    StudentInfo,
+};
