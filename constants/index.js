1df8ecd41d188fdbddd8a5e3df454c74f9927b60
Aashir Hussain <husaashi@sheridancollege.ca>
2022-04-17 12:21:03 -0400

    Coded the index.js file

---
+++ constants/index.js
@@ -0,0 +1,30 @@
+
+export const amex  = require ('../assets/icons/amex.png');
+export const back = require('../assets/icons/back.png');
+export const car  = require ('../assets/icons/car.png');
+export const like  = require ('../assets/icons/like.png');
+export const list  = require ('../assets/icons/list.png');
+export const location  = require ('../assets/icons/location.png');
+export const mastercard  = require ('../assets/icons/mastercard.png');
+export const nearby  = require ('../assets/icons/nearby.png');
+export const pin  = require ('../assets/icons/pin.png');
+export const search  = require ('../assets/icons/search.png');
+export const star  = require ('../assets/icons/star.png');
+export const user  = require ('../assets/icons/user.png');
+export const visa  = require ('../assets/icons/visa.png');
+
+export default {
+    amex,
+    back,
+    car,
+    like,
+    list,
+    location,
+    mastercard,
+    nearby,
+    pin,
+    search,
+    star,
+    user,
+    visa,
+}
