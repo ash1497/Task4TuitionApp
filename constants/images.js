2de8a3fe76e4276753ab945d6375dc8d4d5dbf1d
Zahaak Khan <khazahaa@sheridancollege.ca>
2022-04-17 13:19:45 -0400

    added exports and created basic home template for app.js

--- constants/images.js
+++ constants/images.js
@@ -1,10 +1,7 @@
-
-export const avatar1 = require ('../assets/images/avatar1.jpg');
-export const avatar2 = require ('../assets/images/avatar-2.jpg');
-
-
+export const avatar1 = require('../assets/images/avatar1.jpg');
+export const avatar2 = require('../assets/images/avatar-2.jpg');
 
 export default {
-    avatar1,
-    avatar2,
-}
+  avatar1,
+  avatar2,
+};
--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
-import icons from "./icons";
-import images from "./images";
+import icons from './icons';
+import images from './images';
 import GOOGLE_API_KEY from './maps';
 import {COLORS, SIZES, FONTS} from './theme';
 
-export {icons, images, GOOGLE_API_KEY, COLORS, SIZES, FONTS };
+export {icons, images, GOOGLE_API_KEY, COLORS, SIZES, FONTS};
