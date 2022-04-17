4a04c79e5b53a5d0132dee218fc2c868978c7db9
Zahaak Khan <khazahaa@sheridancollege.ca>
2022-04-17 13:25:52 -0400

    changed file name to match

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {Home, StudentInfo, Location} from './screens';
+import {Home, StudentInfo, Location} from './displays';
 
 const Stack = createStackNavigator();
 const App = () => {
