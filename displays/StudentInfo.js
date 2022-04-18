66eb17c4a96f16e7a07ee4a0b133eff476149d56
Zahaak Khan <khazahaa@sheridancollege.ca>
2022-04-18 18:51:52 -0400

    added top naviagtor to student info page

--- displays/StudentInfo.js
+++ displays/StudentInfo.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import {
+  StyleSheet,
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
+import {isIphoneX} from 'react-native-iphone-x-helper';
+import {icons, COLORS, SIZES, FONTS} from '../constants';
 
-import {View, Text} from 'react-native';
+const StudentInfo = ({route, navigation}) => {
+  const [students, setStudents] = React.useState(null);
+  const [currentLocation, setCurrentLocation] = React.useState(null);
+  React.useEffect(() => {
+    let {item, currentLocation} = route.params;
+    setStudents(item);
+    setCurrentLocation(currentLocation);
+  }, [route.params]);
 
-const StudentInfo = () => {
-  return (
-    <View>
-      <Text>Studentinfo</Text>
-    </View>
-  );
+  function renderHeader() {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={{
+            width: 60,
+            paddingLeft: 20,
+            justifyContent: 'center',
+          }}
+          onPress={() => navigation.goBack()}>
+          <Image
+            source={icons.back}
+            resizeMode="contain"
+            style={{
+              width: 40,
+              height: 40,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.secondary,
+  },
+});
 
 export default StudentInfo;
