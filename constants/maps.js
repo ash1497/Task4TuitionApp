4aa1393449b3b4daf7e733f68523aee8e4ea2f01
Aashir Hussain <74316778+ash1497@users.noreply.github.com>
2025-10-18 18:33:06 -0400

    Update GOOGLE_API_KEY for privacy
    Replaced Google API key with a placeholder for privacy.

--- constants/maps.js
+++ constants/maps.js
@@ -1,5 +1,5 @@
 // this key was created using google clound maps platform generating a key.
-const GOOGLE_API_KEY = 'AIzaSyB8D-5BVnnPrsRidPxOcKQ-3kTW1tvPQFc';
+const GOOGLE_API_KEY = 'Removed For Privacy reasons';
 export default GOOGLE_API_KEY;
 
 // Backup API key: AIzaSyBcGmwW3qb_UBamNlF5KIicAI7McWbSh14
